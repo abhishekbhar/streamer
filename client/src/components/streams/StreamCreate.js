24283b1c2a2fa226354a25f044d8b917b031fa83
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-02-02 10:26:15 +0530

    added create stream action, switched to file to replace sction type string

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, formValues, reduxForm } from 'redux-form'; 
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -32,8 +34,8 @@ class StreamCreate extends React.Component {
     }
 
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit =(formValues) => {
+        this.props.createStream(formValues);
     }
 
 
@@ -63,7 +65,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
